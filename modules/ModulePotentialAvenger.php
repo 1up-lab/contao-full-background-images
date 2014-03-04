a5f817faec782c54a26034c618e1a0ab81460374
David Greminger <david.greminger@1up.io>
2014-03-04 19:24:38 +0100

    Added main module class

---
+++ modules/ModulePotentialAvenger.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Oneup;
+
+class ModulePotentialAvenger extends \Module
+{
+    protected $strTemplate = 'potential_avenger';
+
+    public function generate()
+    {
+        return parent::generate();
+    }
+
+    protected function compile()
+    {
+
+    }
+}
