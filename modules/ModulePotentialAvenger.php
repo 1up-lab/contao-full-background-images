4756aff35793d6971d3e39d118840ee6283e0ebc
David Greminger <david.greminger@1up.io>
2014-03-05 18:04:20 +0100

    Read files in module

--- modules/ModulePotentialAvenger.php
+++ modules/ModulePotentialAvenger.php
@@ -4,15 +4,43 @@ namespace Oneup;
 
 class ModulePotentialAvenger extends \Module
 {
+    protected $objFiles;
     protected $strTemplate = 'potential_avenger';
 
     public function generate()
     {
+        global $objPage;
+
+        $objPage->paSRC = deserialize($objPage->paSRC);
+
+        // Return if there are no files
+        if (!is_array($objPage->paSRC) || empty($objPage->paSRC) || $objPage->usePotentialAvenger === '')
+        {
+            return '';
+        }
+
+        // Get the file entries from the database
+        $this->objFiles = \FilesModel::findMultipleByUuids($objPage->paSRC);
+
+        if ($this->objFiles === null)
+        {
+            if (!\Validator::isUuid($objPage->paSRC[0]))
+            {
+                return '<p class="error">'.$GLOBALS['TL_LANG']['ERR']['version2format'].'</p>';
+            }
+
+            return '';
+        }
+
         return parent::generate();
     }
 
     protected function compile()
     {
+        global $objPage;
 
+        if ($objPage && $objPage->usePotentialAvenger) {
+            
+        }
     }
 }
