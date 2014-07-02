b9072f65faf037cad1cb213abb3feffe84d70db6
David Greminger <david.greminger@1up.io>
2014-07-02 17:49:36 +0200

    Fix selector

--- assets/js/fullbackground.js
+++ assets/js/fullbackground.js
@@ -1,6 +1,5 @@
 ;(function ($) {
     $(document).ready(function() {
-        var body = document.getElementsByTagName("body");
-        body[0].dispatchEvent(new Event("load-fullbg"));
+        window.dispatchEvent(new Event("load-fullbg"));
     });
 })(jQuery);
